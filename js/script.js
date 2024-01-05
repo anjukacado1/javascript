9b1f3fb66f9896b9400900dc4e79c78eb93dfef3
Anjuka P <anjukacad01@brightskills.com>
2024-01-05 18:12:07 +0530

    operators of js file  added

---
+++ js/script.js
@@ -0,0 +1,84 @@
+alert("WElcome to JS");
+
+console.log("welcome to js");
+console.log(10);
+console.log(10.10);
+console.log({fname:"Anjuka",gender:"Female"});
+
+console.table({fname:"Anjuka",gender:"Female"});
+//print Array
+console.log([5,10,25,15]);
+console.table([5,10,25,15]);
+//to print Boolean
+console.log(true);
+//print in console with variable decleration
+var a=5;
+let b=7;
+const c=10;
+console.log(a);
+console.log(b);
+console.log(c);
+
+//reasign
+
+a=6;
+console.log(a);
+
+b=8;
+console.log(b);
+//re-declearation
+
+var a=25;
+console.log(a);
+
+var a=6.5;
+console.log(a);
+
+b=8.5;
+console.log(b);
+
+var a="Anjuka";
+console.log(a);
+
+b="Akilan";
+console.log(b);
+
+b=25;
+console.log(b);
+// you cannot redecleration variable used in "let" method
+// you can redecleration variable used in "var" method
+var a=25;
+console.log(a);
+
+//array redecleare
+a={fname:"Akilan",gender:"male"}
+console.log(a);
+console.table(a);
+
+a=[12,11,25,60];
+console.log(a);
+console.table(a);
+
+b={fname:"Akilan",gender:"male"}
+console.log(b);
+console.table(b);
+
+b=[12,11,25,60];
+console.log(b);
+console.table(b);
+
+//console page clear
+//to know what type of values given
+a=1;
+console.log(typeof a);
+
+a=1.5;
+console.log(typeof a);
+
+a="Akilan";
+console.log(a+";"+typeof a);
+
+console.log({fname:"Anjuka",gender:"Female"});
+console.table({fname:"Akilan",Gender:"Male",Mname:"Anju"});
+
+
